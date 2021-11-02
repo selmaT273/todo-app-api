fe80a2e14dc667f1683e15822587368870aa01d0
Stacey Teltser <stacey.teltser@gmail.com>
2021-11-02 16:44:25 -0500

    can update a todo

--- src/controllers/todos/index.ts
+++ src/controllers/todos/index.ts
@@ -30,4 +30,25 @@ const addTodo = async (req: Request, res: Response): Promise<void> => {
   }
 }
 
-export { getTodos, addTodo }
+const updateTodo = async(req: Request, res: Response): Promise<void> => {
+  try {
+    const {
+      params: { id },
+      body,
+    } = req;
+    const updateTodo: ITodo | null = await Todo.findByIdAndUpdate(
+      {_id: id},
+      body
+    );
+    const allTodos: ITodo[] = await Todo.find();
+    res.status(200).json({
+      message: "Todo updated",
+      todo: updateTodo,
+      todos: allTodos,
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
+export { getTodos, addTodo, updateTodo }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getTodos, addTodo } from '../controllers/todos';
+import { getTodos, addTodo, updateTodo } from '../controllers/todos';
 
 const router: Router = Router();
 
 router.get("/todos", getTodos);
 router.post("/add-todo", addTodo);
+router.put("/edit-todo/:id", updateTodo);
 
 export default router
