b37e2aaa90482c58fb31b7678cb5515815e3ed13
Stacey Teltser <stacey.teltser@gmail.com>
2021-11-02 15:42:50 -0500

    server running

--- src/server.ts
+++ src/server.ts
@@ -5,4 +5,21 @@ import cors from 'cors';
 
 const app: Express = express();
 
+const PORT: string | number = process.env.PORT || 3000;
 
+app.use(cors());
+// app.use(todoRoutes);
+
+const uri: string = 'mongodb://localhost:27017/todo';
+const ConnectOptions = { bufferCommands: false, autoCreate: true, autoIndex: false };
+
+mongoose
+  .connect(uri, ConnectOptions)
+  .then(() => 
+    app.listen(PORT, () =>
+      console.log('server is running')
+    )
+  )
+  .catch(err => {
+    throw err;
+  });
